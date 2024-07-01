65512c1f5154752432b08e3cc29d076a6f30da3a
msohaaib <msohaib968505@gmail.com>
2024-07-01 13:26:52 +0500

    updaated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,15 +6,15 @@ import world from "../../public/laptop.jpg";
 
 export default function Header() {
   return (
-    <div className="w-full mx-auto">
+    <div className="w-full mx-auto bg-white">
       {/* Navbar */}
 
       <div className="flex flex-row justify-between py-5 bg-gradient-to-r from-[#D53469] to-[#DAAC51]">
-        <div className="flex flex-row">
+        <div className="md:flex flex-row hidden">
           <img src={logos} alt="logo" className="h-8" />
           <p className="text-2xl font-bold ml-1">Landing</p>
         </div>
-        <div className="md:flex flex-row gap-8 hidden">
+        <div className="md:flex md:flex-row md:gap-8 fixed top-[90%] flex gap-20 left-1/2 -translate-x-1/2 backdrop-blur-sm px-10 py-3 rounded-full">
           <a
             href=""
             className="my-3 text-black hover:text-slate-800 transition-all"
@@ -40,20 +40,6 @@ export default function Header() {
             Action
           </a>
         </div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="size-6 block md:hidden"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
       </div>
 
       {/* Hero Section */}
@@ -77,71 +63,99 @@ export default function Header() {
         </div>
       </div>
 
-        <div className="flex flex-col items-center w-full my-20">
-          <h2 className="text-5xl font-extrabold">Title</h2>
-          <div className="h-1 w-56 mt-3 bg-gradient-to-r from-[#F4CDD9] to-[#F6EAD4]"></div>
+      <div className="flex flex-col items-center w-full my-20">
+        <h2 className="text-5xl font-extrabold">Title</h2>
+        <div className="h-1 w-56 mt-3 bg-gradient-to-r from-[#F4CDD9] to-[#F6EAD4]"></div>
+      </div>
+      <div className="flex flex-col md:flex-row my-10 w-4/5 mx-auto">
+        <div className="w-10/12 md:w-1/2 self-center">
+          <h3 className="text-2xl text-center font-bold">
+            Lorem ipsum dolor sit.
+          </h3>
+          <p className="text-center capitalize mr-14 ml-14">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at
+            ipsum eu nunc commodo posuere et sit amet ligula.
+          </p>
         </div>
-        <div className="flex flex-col md:flex-row my-10 w-4/5 mx-auto">
-          <div className="w-10/12 md:w-1/2 self-center">
-            <h3 className="text-2xl text-center font-bold">Lorem ipsum dolor sit.</h3>
-            <p className="text-center capitalize mr-14 ml-14">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              at ipsum eu nunc commodo posuere et sit amet ligula.
-            </p>
-          </div>
-          <div className="mx-auto w-2/4">
-            <img src={lap} alt="laptop" className="w-full rounded-xl mx-auto" />
-          </div>
+        <div className="mx-auto w-2/4">
+          <img src={lap} alt="laptop" className="w-full rounded-xl mx-auto" />
         </div>
-        <div className="flex flex-col-reverse md:flex-row w-4/5 mx-auto ">
+      </div>
+      <div className="flex flex-col-reverse md:flex-row w-4/5 mx-auto ">
         <div className="mx-auto w-2/4 my-10">
           <img src={world} alt="globe" className="w-full rounded-xl mr-[50%]" />
         </div>
         <div className="w-10/12 md:w-1/2  self-center">
-          <h3 className="text-2xl text-center mx-auto font-bold">Lorem ipsum dolor sit.</h3>
+          <h3 className="text-2xl text-center mx-auto font-bold">
+            Lorem ipsum dolor sit.
+          </h3>
           <p className="text-center capitalize mr-14 ml-14">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
             consequatur neque nisi corporis voluptates porro.
           </p>
         </div>
-        </div>
+      </div>
 
-        <hr />
+      <hr />
 
-        <div className="flex flex-col items-center w-full my-20">
-          <h2 className="text-5xl font-extrabold">Title</h2>
-          <div className="h-1 w-56 mt-3 bg-gradient-to-r from-[#F4CDD9] to-[#F6EAD4]"></div>
-        </div>
-        
-        <div className="grid md:grid-cols-1 lg:grid-cols-3 gap-28 container mx-auto">
-          <div className="shadow-xl px-10">
-            <p className="text-start mb-16 font-thin pt-3">Getting Started</p>
-            <h3 className="text-xl font-bold">Lorem ipsum dolor sit.</h3>
-            <p className="mb-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ex tempore sapiente officia in culpa ipsa quae consectetur ab!</p>
-            <div className="flex items-center justify-start">
-            <a href="" className="mb-10 bg-gradient-to-r from-[#D53469] to-[#DAAC51] text-white font-semibold px-6 py-3 rounded-2xl">Action</a>
-            </div>
-          </div>
-          <div className="shadow-xl px-10">
-            <p className="text-start mb-16 font-thin pt-3">Getting Started</p>
-            <h3 className="text-xl font-bold">Lorem ipsum dolor sit.</h3>
-            <p className="mb-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ex tempore sapiente officia in culpa ipsa quae consectetur ab!</p>
-            <div className="flex items-center justify-center">
-            <a href="" className="mb-10 bg-gradient-to-r from-[#D53469] to-[#DAAC51] text-white font-semibold px-6 py-3 rounded-2xl">Action</a>
-            </div>
-          </div>
-          <div className="shadow-xl px-10">
-            <p className="text-start mb-16 font-thin pt-3">Getting Started</p>
-            <h3 className="text-xl font-bold">Lorem ipsum dolor sit.</h3>
-            <p className="mb-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ex tempore sapiente officia in culpa ipsa quae consectetur ab!</p>
-            <div className="flex items-center justify-end">
-            <a href="" className="mb-10 bg-gradient-to-r from-[#D53469] to-[#DAAC51] text-white font-semibold px-6 py-3 rounded-2xl">Action</a>
-            </div>
+      <div className="flex flex-col items-center w-full my-20">
+        <h2 className="text-5xl font-extrabold">Title</h2>
+        <div className="h-1 w-56 mt-3 bg-gradient-to-r from-[#F4CDD9] to-[#F6EAD4]"></div>
+      </div>
+
+      <div className="grid md:grid-cols-1 lg:grid-cols-3 gap-28 container mx-auto">
+        <div className="shadow-xl px-10">
+          <p className="text-start mb-16 font-thin pt-3">Getting Started</p>
+          <h3 className="text-xl font-bold">Lorem ipsum dolor sit.</h3>
+          <p className="mb-20">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ex
+            tempore sapiente officia in culpa ipsa quae consectetur ab!
+          </p>
+          <div className="flex items-center justify-start">
+            <a
+              href=""
+              className="mb-10 bg-gradient-to-r from-[#D53469] to-[#DAAC51] text-white font-semibold px-6 py-3 rounded-2xl"
+            >
+              Action
+            </a>
           </div>
         </div>
+        <div className="shadow-xl px-10">
+          <p className="text-start mb-16 font-thin pt-3">Getting Started</p>
+          <h3 className="text-xl font-bold">Lorem ipsum dolor sit.</h3>
+          <p className="mb-20">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ex
+            tempore sapiente officia in culpa ipsa quae consectetur ab!
+          </p>
+          <div className="flex items-center justify-center">
+            <a
+              href=""
+              className="mb-10 bg-gradient-to-r from-[#D53469] to-[#DAAC51] text-white font-semibold px-6 py-3 rounded-2xl"
+            >
+              Action
+            </a>
+          </div>
+        </div>
+        <div className="shadow-xl px-10">
+          <p className="text-start mb-16 font-thin pt-3">Getting Started</p>
+          <h3 className="text-xl font-bold">Lorem ipsum dolor sit.</h3>
+          <p className="mb-20">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ex
+            tempore sapiente officia in culpa ipsa quae consectetur ab!
+          </p>
+          <div className="flex items-center justify-end">
+            <a
+              href=""
+              className="mb-10 bg-gradient-to-r from-[#D53469] to-[#DAAC51] text-white font-semibold px-6 py-3 rounded-2xl"
+            >
+              Action
+            </a>
+          </div>
+        </div>
+      </div>
 
-        <div className="container mx-auto">
-        <div className="flex flex-col items-center w-full my-20">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center w-full my-16">
           <h2 className="text-5xl font-extrabold">Pricing</h2>
           <div className="h-1 w-56 mt-3 bg-gradient-to-r from-[#F4CDD9] to-[#F6EAD4]"></div>
         </div>
@@ -152,10 +166,17 @@ export default function Header() {
             <li className="list-none border-b py-4">Things</li>
             <li className="list-none border-b py-4">Things</li>
             <div className="flex-none rounded-b rounded-t-none shadow p-6 overflow-hidden mt-auto">
-              <div className="pt-6 w-full text-3xl text-gray-600 font-bold">£0 <span className="text-base">For one user</span></div>
+              <div className="pt-6 w-full text-3xl text-gray-600 font-bold">
+                £0 <span className="text-base">For one user</span>
+              </div>
               <div className="flex items-center justify-center">
-            <a href="" className="bg-gradient-to-r from-[#D53469] to-[#DAAC51] mx-auto lg:mx-0 hover:underline text-white font-bold rounded-full px-8 py-4 my-6 shadow-lg">Sign Up</a>
-            </div>
+                <a
+                  href=""
+                  className="bg-gradient-to-r from-[#D53469] to-[#DAAC51] mx-auto lg:mx-0 hover:underline text-white font-bold rounded-full px-8 py-4 my-6 shadow-lg"
+                >
+                  Sign Up
+                </a>
+              </div>
             </div>
           </div>
           <div className="text-center md:w-[35%] border">
@@ -165,10 +186,17 @@ export default function Header() {
             <li className="list-none border-b py-4">Things</li>
             <li className="list-none border-b py-4">Things</li>
             <div className="flex-none rounded-b rounded-t-none shadow p-6 overflow-hidden mt-auto">
-              <div className="pt-6 w-full text-3xl text-gray-600 font-bold">£x.99 <span className="text-base">/ per year</span></div>
+              <div className="pt-6 w-full text-3xl text-gray-600 font-bold">
+                £x.99 <span className="text-base">/ per year</span>
+              </div>
               <div className="flex items-center justify-center">
-            <a href="" className="bg-gradient-to-r from-[#D53469] to-[#DAAC51] mx-auto lg:mx-0 hover:underline text-white font-bold rounded-full px-8 py-4 my-6 shadow-lg">Sign Up</a>
-            </div>
+                <a
+                  href=""
+                  className="bg-gradient-to-r from-[#D53469] to-[#DAAC51] mx-auto lg:mx-0 hover:underline text-white font-bold rounded-full px-8 py-4 my-6 shadow-lg"
+                >
+                  Sign Up
+                </a>
+              </div>
             </div>
           </div>
           <div className="text-center md:w-[20%] border my-auto">
@@ -177,15 +205,81 @@ export default function Header() {
             <li className="list-none border-b py-4">Things</li>
             <li className="list-none border-b py-4">Things</li>
             <div className="flex-none rounded-b rounded-t-none shadow p-6 overflow-hidden mt-auto">
-              <div className="pt-6 w-full text-3xl text-gray-600 font-bold">£x.99 <span className="text-base">/ per year</span></div>
+              <div className="pt-6 w-full text-3xl text-gray-600 font-bold">
+                £x.99 <span className="text-base">/ per year</span>
+              </div>
               <div className="flex items-center justify-center">
-            <a href="" className="bg-gradient-to-r from-[#D53469] to-[#DAAC51] mx-auto lg:mx-0 hover:underline text-white font-bold rounded-full px-8 py-4 my-6 shadow-lg">Sign Up</a>
-            </div>
+                <a
+                  href=""
+                  className="bg-gradient-to-r from-[#D53469] to-[#DAAC51] mx-auto lg:mx-0 hover:underline text-white font-bold rounded-full px-8 py-4 my-6 shadow-lg"
+                >
+                  Sign Up
+                </a>
+              </div>
             </div>
           </div>
         </div>
+      </div>
 
+      <div className="flex flex-col justify-center bg-gradient-to-r from-[#D53469] to-[#DAAC51] py-20 my-20">
+          <h2 className="text-5xl font-extrabold text-white text-center">Call to Action</h2>
+          <div className="h-1 w-56 mt-3 bg-gradient-to-r from-[#F4CDD9] mx-auto to-[#F6EAD4]"></div>
+        <p className="text-3xl text-white my-10 text-center">
+          Main Hero Message to sell product!
+        </p>
+        <a
+            href=""
+            className="font-semibold mx-auto bg-white hover:underline px-6 py-3 items-center rounded-full"
+          >
+            Action!
+          </a>
+      </div>
+
+
+      <footer className="w-full mx-auto">
+        <div className="flex flex-col md:flex-row md:justify-around justify-start ml-5">
+          <div className="flex flex-row mb-5">
+          <img src={logos} alt="logo" className="h-8" />
+          <p className="text-2xl font-bold ml-1">Landing</p>
         </div>
+          
+          <div className="flex flex-col my-2">
+            <div className="uppercase font-thin mb-2">Links</div>
+            <div className="flex flex-row gap-4 md:flex-col">
+            <a href="">FAQ</a>
+            <a href="">Help</a>
+            <a href="">Support</a>
+            </div>
+          </div>
+          <div className="flex flex-col my-2">
+            <div className="uppercase font-thin mb-2">Legal</div>
+            <div className="flex flex-row gap-4 md:flex-col">
+            <a href="">Terrms</a>
+            <a href="">Privacy</a>
+            </div>
+          </div>
+          <div className="flex flex-col my-2">
+            <div className="uppercase font-thin mb-2">Social</div>
+            <div className="flex flex-row gap-4 md:flex-col">
+            <a href="">Faebook</a>
+            <a href="">Linkdin</a>
+            <a href="">Twitter</a>
+            </div>
+          </div>
+          <div className="flex flex-col mb-2">
+            <div className="uppercase font-thin my-2">Company</div>
+            <div className="flex flex-row gap-4 md:flex-col">
+            <a href="">Offical Blog</a>
+            <a href="">About Us</a>
+            <a href="">Contact</a>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center text-center my-3 bg-gradient-to-r from-[#D53469] to-[#DAAC51] text-white">
+          &copy; Copyright Reserved
+        </div>
+
+      </footer>
 
     </div>
   );
